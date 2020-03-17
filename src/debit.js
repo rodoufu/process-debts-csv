550b5336b2f028cb2e27ca32565919e8a8861a3d
Rodolfo Araujo <rodoufu@gmail.com>
2020-03-16 22:26:56 -0300

    One step.

--- src/debit.js
+++ src/debit.js
@@ -1,13 +1,67 @@
+class UnexpectedFormatError extends Error {
+	constructor(message) {
+		super(message);
+	}
+}
+
+class InvalidStringError extends Error {
+	constructor(message) {
+		super(message);
+	}
+}
+
+class InvalidValueError extends Error {
+	constructor(message) {
+		super(message);
+	}
+}
+
 function separateLine(line, separator = ',') {
+	if (!line) {
+		throw new UnexpectedFormatError("null line");
+	}
 	line = line.trim();
 	if (line.length === 0) {
-		//throw Error("Empty line");
+		throw new UnexpectedFormatError("Empty line");
 	}
-	let values = line.split(separator);
-	return line;
+
+	const linePat = separator === ',' ?
+		new RegExp(/^(.+),(.+),(.+)$/) :
+		new RegExp(/^(.+);(.+);(.+)$/);
+	let found = line.match(linePat);
+	if (!found || found.index !== 0 || found.length !== 4) {
+		throw new UnexpectedFormatError('Unexpected format');
+	}
+
+	const sep = ['"', "'"];
+	const namePat = [new RegExp(/^\s*"(\S*)"\s*$/), new RegExp(/^\s*'(\S*)'\s*$/)];
+	for (let j = 1; j < 4; ++j) {
+		for (let i = 0; i < namePat.length; ++i) {
+			let theFound = found[j].match(namePat[i]);
+			if (theFound) {
+				found[j] = theFound[1];
+			} else if (found[j].indexOf(sep[i]) !== -1) {
+				throw new InvalidStringError(`Unexpected format, missing ${sep[i]}`);
+			}
+		}
+	}
+
+	let value = Number(found[3]);
+
+	if (isNaN(value)) {
+		throw new InvalidValueError(`Unexpected format, value is not a number: '${found[3].trim()}'`);
+	}
+
+	return {
+		'from': found[1],
+		'to': found[2],
+		'value': value,
+	};
 }
 
 module.exports = {
-	files: [],
-	separateLine
-}
+	UnexpectedFormatError,
+	InvalidStringError,
+	InvalidValueError,
+	separateLine,
+};
--- test/debit.js
+++ test/debit.js
@@ -1,9 +1,88 @@
 const assert = require('assert');
+const debit = require('../src/debit.js');
 
-describe('Array', function () {
-	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function () {
-			assert.equal([1, 2, 3].indexOf(4), -1);
+describe('separateLine', () => {
+	describe('Invalid', () => {
+		it('Empty line', () => {
+			assert.throws(() => { debit.separateLine(); }, debit.UnexpectedFormatError, "undefined");
+			assert.throws(() => { debit.separateLine(null); }, debit.UnexpectedFormatError, "Null");
+			assert.throws(() => { debit.separateLine(''); }, debit.UnexpectedFormatError, "Empty line");
+		});
+		it('Unexpected format', () => {
+			assert.throws(() => { debit.separateLine('a'); }, debit.UnexpectedFormatError, "One char");
+			assert.throws(() => { debit.separateLine('a,b'); }, debit.UnexpectedFormatError, "2 columns");
+			assert.throws(() => { debit.separateLine('a,b,c,d'); }, debit.UnexpectedFormatError, "4 columns");
+		});
+		it('Unexpected format, invalid string', () => {
+			assert.throws(() => { debit.separateLine('"a,b,4'); }, debit.InvalidStringError, '"a');
+			assert.throws(() => { debit.separateLine('a",b,5'); }, debit.InvalidStringError, 'a"');
+			assert.throws(() => { debit.separateLine('a,"b,4'); }, debit.InvalidStringError, '"b');
+			assert.throws(() => { debit.separateLine('a,b",5'); }, debit.InvalidStringError, 'b"');
+		});
+		it('Unexpected format, not a number', () => {
+			assert.throws(() => { debit.separateLine('a,b,c'); }, debit.InvalidValueError, "c");
+			assert.throws(() => { debit.separateLine('a,b,5.f'); }, debit.InvalidValueError, "5.f");
+		});
+	});
+	describe('Valid', () => {
+		it('Simple', () => {
+			let line = debit.separateLine('a,b,5');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5);
+
+			line = debit.separateLine('a,b,5.5');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5.6);
+		});
+		it('Quotation marks', () => {
+			let line = debit.separateLine('"a",b,5');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5);
+
+			line = debit.separateLine('a,"b",5.5');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5.6);
+
+			line = debit.separateLine('a,b,"5.5"');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5.6);
+
+			line = debit.separateLine('a,"b","5.5"');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5.6);
+
+			line = debit.separateLine('"a","b","5.5"');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5.6);
+		});
+
+		it('Quotation marks with comma', () => {
+			let line = debit.separateLine('"a,b",b,5');
+			assert.equal(line.from, 'a,b');
+			assert.equal(line.to, 'b');
+			assert.equal(line.value, 5);
+
+			line = debit.separateLine('a,"b,c",5.5');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b,c');
+			assert.equal(line.value, 5.6);
+
+			line = debit.separateLine('a,"b,c","5.5"');
+			assert.equal(line.from, 'a');
+			assert.equal(line.to, 'b,c');
+			assert.equal(line.value, 5.6);
+
+			line = debit.separateLine('"a,d","b,c","5.5"');
+			assert.equal(line.from, 'a,d');
+			assert.equal(line.to, 'b,c');
+			assert.equal(line.value, 5.6);
 		});
 	});
 });
